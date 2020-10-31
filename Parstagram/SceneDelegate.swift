493d5b36cb2dc3364c48e5908c3fe6a7b502c955
Priyanka Balwadkar <pdbalwad@syr.edu>
2020-10-30 22:06:07 -0400

    Staying logged in implemented

--- Parstagram/SceneDelegate.swift
+++ Parstagram/SceneDelegate.swift
@@ -14,6 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        //staying logged in
         if PFUser.current() != nil{
             let main = UIStoryboard(name: "Main", bundle: nil)
             let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
