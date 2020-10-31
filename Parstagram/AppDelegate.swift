898f8daa90b59fed488c705c02626f72dd63a03c
Priyanka Balwadkar <pdbalwad@syr.edu>
2020-10-30 22:02:36 -0400

    Pull to Refresh and Continuous scroll implemented.

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -8,10 +8,11 @@
 import UIKit
 import Parse
 
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+   
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -22,10 +23,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                     $0.server = "https://parseapi.back4app.com"
             }
             Parse.initialize(with: parseConfig)
-            
+       
         
         return true
     }
+    
+    
 
     // MARK: UISceneSession Lifecycle
 
--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -12,43 +12,61 @@ import AlamofireImage
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
-    
+    var user = PFUser()
     var posts = [PFObject]()
+    let myRefreshControl = UIRefreshControl()
+    var numberOfPosts: Int!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
         
+        super.viewDidLoad()
+        numberOfPosts = 10
         tableView.delegate = self
         tableView.dataSource = self
-
+        
+        loadPosts()
+        
+        myRefreshControl.addTarget(self, action: #selector(onRefresh), for: .valueChanged)
+        
+        tableView.refreshControl = myRefreshControl
+        
+        
         // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    func loadPosts()
+    {
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author")
+        
+        query.limit = 10
+        
+        query.findObjectsInBackground {(posts, error) in
+        if posts != nil{
+         
+            self.posts = posts!.reversed()
+            self.tableView.reloadData()
+               
+            }
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let query = PFQuery(className:"Posts")
-        query.includeKey("author")
-        
-        query.limit = 20
-        
-        query.findObjectsInBackground {(posts, error) in
-            if posts != nil{
-                self.posts = posts!
-                self.tableView.reloadData()
-            }
-            
-    }
+        loadPosts()
+        //self.tableView.reloadData()
     }
     
     
     @IBAction func onLogoutButton(_ sender: Any) {
         
+        //let user = posts[0]["author"] as PFUser!
         PFUser.logOut()
         self.dismiss(animated: true, completion: nil)
         
     }
-    
-    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return posts.count
@@ -57,19 +75,66 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let post = posts[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
-        let user = post["author"] as! PFUser
+        user = post["author"] as! PFUser
        
         cell.usernameLabel.text = user.username
         cell.captionLabel.text = post["caption"] as? String
         
         let imageFile = post["image"] as! PFFileObject
         let urlString = imageFile.url!
-        let url = URL(string: urlString)
-        cell.postView.af_setImage(withURL: url!)
+        let url = URL(string: urlString)!
+        
+        cell.postView.af_setImage(withURL: url)
         
         return cell
         
     }
+    
+    
+    
+    @objc func onRefresh()
+    {
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author")
+        
+        query.limit = 10
+        
+        query.findObjectsInBackground {(posts, error) in
+            if posts != nil {
+                self.posts = posts!.reversed()
+                
+                self.tableView.reloadData()
+                self.myRefreshControl.endRefreshing()
+            }
+        }
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath){
+        if indexPath.row + 1 == posts.count{
+            loadMorePosts()
+        }
+    }
+    
+    
+    func loadMorePosts(){
+        
+        numberOfPosts = numberOfPosts + 10
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author")
+        query.limit = numberOfPosts
+    
+        query.findObjectsInBackground {(posts, error) in
+            if posts != nil {
+                
+                self.posts.removeAll()
+                self.posts = posts!.reversed()
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
 
     /*
     // MARK: - Navigation
